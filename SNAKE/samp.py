53aa21c6dbab53af6b5b3f0016eeb2865337bd73
SHAJUK10 <129306450+SHAJUK10@users.noreply.github.com>
2023-03-29 20:33:25 +0530

    Add files via upload

---
+++ SNAKE/samp.py
@@ -0,0 +1,42 @@
+# print('hellooooo')
+# a=10.3
+list1=['aaaa',12,5.3,'True']
+# li2=tuple(list1)
+
+# print(list1[0])
+# print(type(a))
+
+
+# tuple11=("orange","mango","pappaya")
+# a=list(tuple11)
+# a[2]=12
+# tuple11=list(a)
+# print(tuple11)
+
+
+
+# dist1 ={'phone':9,70:10,}
+# dist1[70]='aaaa'
+# print(dist1)
+
+
+# set1={'sss',111,676767}
+# print(set1[0])
+# print(set1)
+
+a=10
+b=20
+c=78
+if a>b and a>c:
+    print("a is great")
+elif b>a and b>c:
+    print("b is great")
+else: print("c is great")
+
+for i in list1:
+    print(i)
+
+a=0
+while a<=10:
+    print(a)
+    a=a+1
